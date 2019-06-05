0f61add313d88d2c1d06ae9c9fa5fafdcd79a8cb
misstons <50846452+misstons@users.noreply.github.com>
2019-06-05 22:42:41 +0200

    Реализация вычисления функции при помощи метода в классе Point
    Задание 2 пункт 4

--- sandbox/src/main/java/ru/stqa/java_study/sandbox/DistancePoint2.java
+++ sandbox/src/main/java/ru/stqa/java_study/sandbox/DistancePoint2.java
@@ -5,12 +5,8 @@ public class DistancePoint2 {
 
         Point p1 = new Point(5, 6);
         Point p2 = new Point(10, 15);
-        System.out.println("Расстояние между точками = " + distance(p1, p2));
+        System.out.println("Расстояние между точками = " + p1.distance(p2));
 
     }
-        public static double distance(Point p1, Point p2) {
-            double d =((p1.x - p2.x)*(p1.x - p2.x)) + ((p1.y - p2.y)*(p1.y - p2.y));
-            return Math.sqrt(d);
-            //System.out.println(Math.sqrt(d));
-    }
+
 }
